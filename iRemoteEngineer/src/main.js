95708f6bd08bd88513388d04f2ad67bf6b7e7ae8
Mateusz Woźniak <mateusz.wozniak.0211@gmail.com>
2025-04-10 22:32:47 +0200

    chose aura theme

--- iRemoteEngineer/src/main.js
+++ iRemoteEngineer/src/main.js
@@ -2,9 +2,20 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
 
 import App from './App.vue'
 
 const app = createApp(App);
-app.use(PrimeVue);
+app.use(PrimeVue, {
+    // Default theme configuration
+    theme: {
+        preset: Aura,
+        options: {
+            prefix: 'p',
+            darkModeSelector: 'system',
+            cssLayer: false
+        }
+    }
+ });
 app.mount('#app');
